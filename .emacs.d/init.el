bb2fbd0c165ae48e38d14db99304b73a58a875bc
Gabriele Camarda <gabriele@remote.com>
2021-11-11 15:40:27 +0000

    remap keys for dumb jump

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -170,17 +170,18 @@
                   (javascript-mode  "{" "}" "/[*/]" nil)))))
 
 (use-package dumb-jump
-  :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go)
-         ("M-g i" . dumb-jump-go-prompt)
-         ("M-g x" . dumb-jump-go-prefer-external)
-         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :bind (("C-c C-g o" . dumb-jump-go-other-window)
+         ("C-c C-g j" . dumb-jump-go)
+         ("C-c C-g i" . dumb-jump-go-prompt)
+         ("C-c C-g x" . dumb-jump-go-prefer-external)
+         ("C-c C-g z" . dumb-jump-go-prefer-external-other-window))
   :config (setq dumb-jump-selector 'ivy)
   :ensure)
 
 ;; Global hooks
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'xref-backend-definitions #'dumb-jump-xref-activate)
 
 ;; Custom settings
 (load "~/.emacs.d/custom_settings.el")
